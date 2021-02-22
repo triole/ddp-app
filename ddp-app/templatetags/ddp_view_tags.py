af1d9b33bfd60781f90de3025df88b63f99d362f
olaf michaelis <o.mic@eb.de>
2021-02-22 11:37:25 +0100

    Add md and docx renderer

--- ddp-app/templatetags/ddp_view_tags.py
+++ ddp-app/templatetags/ddp_view_tags.py
@@ -1,14 +1,18 @@
-import logging
+# import logging
 import re
 from os.path import join as pj
 from os.path import realpath
-from urllib import parse
 
 import requests
 import yaml
 from django import template
+from django.utils.safestring import mark_safe
 
-log = logging.getLogger(__name__)
+import pypandoc
+
+# from urllib import parse
+
+# log = logging.getLogger(__name__)
 register = template.Library()
 
 
@@ -23,32 +27,83 @@ class Conf():
     def load_config(self):
         with open(self.conf_file, 'r') as stream:
             try:
-                return(yaml.safe_load(stream))
+                r = yaml.safe_load(stream)
+                self.log('Config loaded: ' + str(r))
+                return r
             except yaml.YAMLError as exc:
-                print(exc)
+                self.log(exc)
 
     def abs_url(self, str):
-        return parse.urljoin(self.get_base_url(), str)
+        if str.startswith('/'):
+            str = str[1:]
+        return self.conf_base_url() + self.conf_content_type() + '/' +\
+            str + '.' + self.conf_content_type()
+        # return parse.urljoin(
+        #     self.conf_base_url(), self.conf_content_type() + '/',
+        #     '/' + str + '.' + self.conf_content_type()
+        # )
 
-    def get_base_url(self):
-        return self.conf['base_url']
+    def conf_base_url(self):
+        r = self.conf['base_url']
+        if r.endswith('/') is False:
+            r += '/'
+        return r
+
+    def conf_content_type(self):
+        return self.conf['content_type']
+
+    def log(self, str):
+        print('=== DDP Log === ' + str)
 
 
 c = Conf()
 
 
-@register.simple_tag(takes_context=True)
-def httpget(context, url):
+def download_file(url, tempfile):
     furl = c.abs_url(url)
-    print('Request ' + url)
-    r = requests.get(furl, auth=('user', 'pass'))
-    if r.status_code == 200:
-        return r.text
+    c.log('Download ' + furl + ' to ' + tempfile)
+    res = requests.get(furl)
+    file = open(tempfile, 'wb')
+    file.write(res.content)
+    file.close
+
+
+def request(url):
+    r = ''
+    furl = c.abs_url(url)
+    c.log('Request ' + furl)
+    res = requests.get(furl, auth=('user', 'pass'))
+    if res.status_code == 200:
+        r = res.text
     else:
-        log.error("View httpget failed %s, %s", furl, r.status_code)
-    return ''
+        c.log('View httpget failed ' + furl + str(res.status_code))
+    return r
 
 
-@register.simple_tag()
-def hello():
-    return 'world man'
+def serve_md(str):
+    return mark_safe(
+        pypandoc.convert_text(str, 'html', format='md')
+    )
+
+
+def serve_docx(filename):
+    r = ''
+    # if file does not exist, pandoc throws an exception
+    try:
+        r = pypandoc.convert_file(filename, 'html', format='docx')
+    except RuntimeError:
+        pass
+    return mark_safe(r)
+
+
+@register.simple_tag(takes_context=False)
+def get(url):
+    if c.conf_content_type() == 'md':
+        c.log('Serve md')
+        str = request(url)
+        return serve_md(str)
+    if c.conf_content_type() == 'docx':
+        c.log('Serve docx')
+        tempfile = pj('/tmp/', url + '.docx')
+        download_file(url, tempfile)
+        return serve_docx(tempfile)
